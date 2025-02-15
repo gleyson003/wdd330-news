3cc7ca9de3d9e551ecfc0236c4d19a91e15a9028
gleyson003 <lop23031@byui.edu>
2025-02-15 01:45:09 -0300

    Home page of news project done.

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,7 @@ function loadHTML(id, url, callback) {
 // Use main dynamic
 function loadPage(page) {
     loadHTML("content", `/pages/${page}`);
+    history.pushState({ page: page }, "", `?page=${page}`);
 }
 
 function updateFooter() {
@@ -33,5 +34,53 @@ function updateFooter() {
 document.addEventListener("DOMContentLoaded", () => {
     loadHTML("header", "public/partials/header.html");
     loadHTML("footer", "public/partials/footer.html", updateFooter);
-    loadPage("home.html");
+
+    const params = new URLSearchParams(window.location.search);
+    const page = params.get("page") || "home.html";
+
+    loadPage(page);
 });
+
+//bitcoin api
+function bitcoinPrice () {
+    const url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        if (data && data.bitcoin && data.bitcoin.usd) {
+            document.getElementById("bitcoin-price").textContent = `$ ${data.bitcoin.usd.toFixed(2)}`
+        } else {
+            console.error("Error trying get the bitcoin price.")
+        }
+    })
+}
+
+function convertCurrency() {
+    const amount = document.getElementById("amount").value;
+    const fromCurrency = document.getElementById("from-currency").value;
+    const toCurrency = document.getElementById("to-currency").value;
+
+    if (!amount || amount <= 0) {
+        alert("Please enter a valid amount.");
+        return;
+    }
+
+    const apiKey = "18e72fe75bc4bd57f951b8f7";  // Substitua pela sua chave da API
+    const url = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${fromCurrency}/${toCurrency}/${amount}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.conversion_result) {
+                document.getElementById("converted-value").textContent = `${data.conversion_result.toFixed(2)} ${toCurrency}`;
+            } else {
+                console.error("Error getting exchange rate.");
+                document.getElementById("converted-value").textContent = "Error!";
+            }
+        })
+        .catch(error => console.error("API request error:", error));
+}
+
+
+document.addEventListener("DOMContentLoaded", bitcoinPrice);
